fbc823e74946b34de45bd8645accab9cfe821162
Spencer Pingitore <silentfoe6@gmail.com>
2023-05-10 02:15:05 -0400

    finished styling search and watchlist page without any movie data for both mobile and large screens. Added JS function to show movie data, img, title, etc... in the DOM. Need to still work on getting the movie data to display properly and add movies to local storage

--- main.js
+++ main.js
@@ -1,43 +1,78 @@
 //for the input field text
 let inputVal = document.querySelector('input')
-
+// bottom half of main page with the "start exploring text"
 moviesSection = document.getElementById('show-movies')
 
-// maybe should use the 's' function to search with the API instead of the 't'??? 's' pulls up all movie titles with the search word in it but with less data. 
-// example: http://www.omdbapi.com/?apikey=1a68644e&s=<search>&t=<title>
-// figure out a way to call the API again with the movie title from the 's' search in order to populate more data about the movie 
-// need a way to loop through the search results and then call the API with each movie id number
 
 document.querySelector('button').addEventListener('click', async () => {
-    let res = await fetch(`http://www.omdbapi.com/?apikey=1a68644e&s=${inputVal.value.split(' ').join('+')}`)
-    let data = await res.json()
-
-    // need to use multiple functions in order to call data from the API using the path below. Need to loop through the data.Search array and get the movieID and then pull the data from the movie id into the watchlist render. 
-    // console.log(data.Search[1].Title)
     
+    let movieIMDBNumArray = [] // setting an empty array to hold all the movie title IMDB ID's
 
-    // looping through the data that comes back from the search and then putting that data into a new function to call the API to get the full movie data. 
-    for(let i = 0; i < data.Search.length; i++){
-        getFullMovieData(data.Search[i].imdbID)
+    try {
+        let res = await fetch(`http://www.omdbapi.com/?apikey=1a68644e&s=${inputVal.value.split(' ').join('+')}`)
+        let data = await res.json()
+        
+        for(let i = 0; i < data.Search.length; i++){ // looping through the data that comes back from the search and then putting that data into an array
+            movieIMDBNumArray.push(data.Search[i].imdbID)
+        }
+
+    } catch (error) {
+        
+        
+        moviesSection.innerHTML = `<div class="not-found-container"><p class="not-found">Unable to find what you're looking for. Please try another search.</p></div>`
+        
     }
     
+    getFullMovieData(movieIMDBNumArray) // calling the array filled with the movie title IMDB ID's
+  
 })
 
 
 // calling the API for each movie to get the full movie object back from the inital search
-async function getFullMovieData(movie) {
-    let res = await fetch(`http://www.omdbapi.com/?apikey=1a68644e&i=${movie}`)
-    let data = await res.json()
-    displayMovieData(data)
+async function getFullMovieData(movieID) {
+
+    let fullMovieDataArray = []
+
+    try {
+
+        for(let i = 0; i < movieID.length; i++){
+            
+            res = await fetch(`http://www.omdbapi.com/?apikey=1a68644e&i=${movieID[i]}`)
+            fullMovieDataArray.push(await res.json()) 
+        }
+        
+        
+    } catch (error) {
+        throw error
+    }
+    
+    console.log(fullMovieDataArray)
+    console.log(fullMovieDataArray[0].Ratings[0].Value)
+    
+    displayMovieData(fullMovieDataArray)
+}
+
+// function below is displaying the movie data in the DOM
+function displayMovieData(movieDataArray) {
+    
+    let movieDataParts = ''
+
+    movieDataArray.map(movie => {
+        movieDataParts += 
+        `<div class="movie-data-container">
+            <img class="movie-img" src=${movie.Poster} />
+            <div class="movie-contents">
+                <div class="movie-title-rating">${movie.Title}${movie.Ratings[0].Value}</div>
+                <div class="movie-time-genre-add">
+                ${movie.Runtime}${movie.Genre}<a href="search.html" id="Add-to-watchlist"><i class=" circle-icon fa-solid fa-circle-plus" style="color: #ffffff;"></i>Watchlist</a>
+                </div>
+                <div class="movie-plot">${movie.Plot}</div>
+            </div>
+        </div>
+        <hr>`
+    })
+
+    moviesSection.innerHTML = movieDataParts
 }
 
 
-
-// showing the movie data in the DOM--currently showing only the movie images. 
-function displayMovieData(movieData) {
-    let movieImg = ''
-
-    movieImg += `<img src="${movieData.Poster}">`
-
-    moviesSection.innerHTML += movieImg
-}
